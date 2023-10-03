a4ed36737ce4a59b789373afed8e7517e378fcc9
devUnais <unaisnizamani598@gmail.com>
2023-10-03 21:20:24 +0500

    some changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-
+const nodemailer = require('nodemailer')
 const PORT = 80;
 const bodyParser = require("body-parser");
 const { initializeApp } = require('firebase/app')
