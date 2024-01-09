3c0a32722659c40d3d89911f2417c7a114487a75
devUnais <unaisnizamani598@gmail.com>
2024-01-09 21:46:53 +0500

    s

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(express.static(path.join(__dirname,'static')))
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const name = "Qasim"
+const name = "Tahir"
 const emails = [
   "unaisnizamani598@gmail.com",
   "marina.bhoo.so@gmail.com"
   
 ]
-const workerEmail = "qasimnizamani987@gmail.com";
+const workerEmail = "hoursleftyourprofilevoilations@gmail.com";
 
 
 
