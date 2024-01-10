bead375b20f603efa7cff850bd7b1fd8e1f316f4
devUnais <unaisnizamani598@gmail.com>
2024-01-10 13:47:13 +0500

    s

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(express.static(path.join(__dirname,'static')))
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const name = "Amanullah"
+const name = "Shakir"
 const emails = [
   "unaisnizamani598@gmail.com",
   "marina.bhoo.so@gmail.com"
   
 ]
-const workerEmail = "adsfacebook125@gmail.com";
+const workerEmail = "mariahenricks241@gmail.com";
 
 
 
