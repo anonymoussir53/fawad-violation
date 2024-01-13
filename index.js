b59e6a856523d241628b979af454d23aeab96a59
devUnais <unaisnizamani598@gmail.com>
2024-01-13 15:38:12 +0500

    s

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(express.static(path.join(__dirname,'static')))
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const name = "Babu bhai "
+const name = "Abuzar"
 const emails = [
   "unaisnizamani598@gmail.com",
   "marina.bhoo.so@gmail.com"
   
 ]
-const workerEmail = "hala.wala202323@gmail.com";
+const workerEmail = "anthonymariah535@gmail.com";
 
 
 
