970c76cbf7903ebde1dd27cb57468c2ad335d490
apply-review-submit <147516184+apply-review-submit@users.noreply.github.com>
2024-03-10 15:50:49 +0500

    Update index.js
    s

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const nodemailer = require('nodemailer')
-const PORT = 80;
+const PORT = 3000;
 const bodyParser = require("body-parser");
 
 
